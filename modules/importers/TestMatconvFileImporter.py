00bd5a67096307f779315310a11265101a58f77c
Carlos B <carlos.baezruiz@gmail.com>
2016-07-29 13:31:17 +0200

    Added test to check parser

--- modules/importers/TestMatconvFileImporter.py
+++ modules/importers/TestMatconvFileImporter.py
@@ -1,9 +1,25 @@
 import unittest
 from MatconvFileImporter import  Importer
+ 
+
 
 class TestImporter(unittest.TestCase):
+    def load_file(self,file_path):
+        text_file = open(file_path,"r")
+        expected_output = text_file.read()
+        text_file.close()
+        return expected_output
+
+
     def testInit(self):
         importer = Importer('../config/Matconv/equivalences.yaml')
         self.assertTrue(importer.equivalences!=None)
     
-
+    def testLoad(self):
+        importer = Importer('../config/Matconv/equivalences.yaml')
+        output = importer.load('../../resources/imagenet-vgg-s.mat')
+       
+        #TODO improve output test
+        expected_output = self.load_file('../../resources/expected_out.txt')
+        self.assertTrue(expected_output==str(output))
+        
